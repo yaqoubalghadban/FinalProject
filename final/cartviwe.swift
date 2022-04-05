3ee778964f4a6698e86aba9b98e50ac5f0e4586c
yaqoubalghadban <yoyob4654@gmail.com>
2022-04-05 23:57:28 +0300

    doneeeee

--- final/cartviwe.swift
+++ final/cartviwe.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct cartviwe: View {
     @EnvironmentObject var cartEvn: CartEnv
-
+    @State var showingAlert = false
     @State var totalAmount = 0.0
     var body: some View {
         ZStack {
@@ -40,11 +40,33 @@ struct cartviwe: View {
                             
                                                 }
                     }
-                    .onDelete(perform: {IndexSet in cartEvn.removeItem(at: IndexSet)})
+                    .onDelete(perform: {IndexSet in
+                        IndexSet.forEach { i in
+                            totalAmount = totalAmount - clubes[0].courses[i].price
+                        }
+                        
+                        cartEvn.removeItem(at: IndexSet)})
                 }.backgroundList(Color("back"))
-          Text("\(totalAmount)")
+                Text("Total Price :   \(totalAmount.formatted())KD")
+                    .font(.title3)
+                    .fontWeight(.regular)
+                
+                Button(action: {
+                    showingAlert.toggle()
+                                    }, label: {
+                    Text("Tap To Order")
+                        .foregroundColor(.white)
+                        .frame(width: 150, height: 55)
+                        .background(Color(hue: 0.603, saturation: 0.806, brightness: 0.443))
+                        .cornerRadius(8)
+                        
+                    
+                }).padding()
+
             }
             
+        }.alert(isPresented: $showingAlert) {
+            Alert(title: Text("Confirm your order"), message: Text("Payment done successfully"), dismissButton: .default(Text("Lets Dive")))
         }
     }
 }
